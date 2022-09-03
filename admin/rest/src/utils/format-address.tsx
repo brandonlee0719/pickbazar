1d1db3b10b12c2a2cbf542f4cb5d12d6790ecd0c
bestmycode <bestmycode610@gmail.com>
2022-09-03 20:53:42 +0900

    init

---
+++ admin/rest/src/utils/format-address.tsx
@@ -0,0 +1,21 @@
+import { UserAddress } from '@/types';
+
+function removeFalsy(obj: any) {
+  return Object.fromEntries(Object.entries(obj).filter(([_, v]) => Boolean(v)));
+}
+
+export function formatAddress(address: UserAddress) {
+  if (!address) return;
+  const temp = ['street_address', 'city', 'state', 'zip', 'country'].reduce(
+    (acc, k) => ({ ...acc, [k]: (address as any)[k] }),
+    {}
+  );
+  const formattedAddress = removeFalsy(temp);
+  return Object.values(formattedAddress)?.map((item: any, index) => {
+    return (
+      <span className="truncate" key={index}>
+        {item}
+      </span>
+    );
+  });
+}
